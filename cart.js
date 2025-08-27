ba81625d9c8950e071e177a6f8816f0f47ddd396
unknown <mijan90786@gmail.com>
2025-08-28 01:25:45 +0600

    Add full JS requirement

--- cart.js
+++ cart.js
@@ -1,16 +1,32 @@
+function getElement(id) {
+    const element = document.getElementById(id)
+    return element
+}
 
+const historyContainer = getElement("call-history-container")
 
-const productContainer =  document.getElementById("product-box")
-productContainer.addEventListener("click", function(e){
-    if(e.target.className.includes("call-btn")){
-        const callButton = e.target ;
-        
+const cartContainer = getElement("dialer-box")
+cartContainer.addEventListener("click", function (e) {
+    if (e.target.className.includes("call-btn")) {
+
+        const coinCount = getElement("coin").innerText
+
+        if (coinCount < 20) {
+            alert("আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।")
+            return
+        }
+
+        const currentCoinCount = Number(coinCount) - Number(20)
+        getElement("coin").innerText = currentCoinCount;
+
+        const callButton = e.target;
+
         const serviceName = callButton.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].innerText
         // console.log(serviceName)
         const hotlineNumber = callButton.parentNode.parentNode.childNodes[3].childNodes[3].childNodes[1].innerText
-        console.log(hotlineNumber)
+        // console.log(hotlineNumber)
         const callTime = new Date().toLocaleTimeString()
-        console.log(callTime)
+        // console.log(callTime)
 
         const newCart = document.createElement("div")
         newCart.innerHTML = ` <div id="call-history" class="add-history flex justify-between bg-green-100 p-2 rounded-xl items-center ">
@@ -22,10 +38,32 @@ productContainer.addEventListener("click", function(e){
                         </div>
                         <div class="text-lg font-medium"> ${callTime}</div>
                     </div>`
-         const cartContainer = document.getElementById("add-cart-container")
-         cartContainer.append(newCart) 
+
+        historyContainer.append(newCart)
         const alertMessage = `📞 Calling ${serviceName} ${hotlineNumber}...`
-         alert(alertMessage)
+        alert(alertMessage)
         //  console.log(cartContainer)
     }
+    if (e.target.className.includes("copy-button")) {
+        const copyCount = getElement("copy-count").innerText
+        const currentCopyCount = Number(copyCount) + Number(1)
+        getElement("copy-count").innerText = currentCopyCount;
+        const copyButton = e.target;
+        let hotlineNumber = copyButton.parentNode.parentNode.childNodes[3].childNodes[3].childNodes[1].innerText
+        navigator.clipboard.writeText(hotlineNumber)
+        const message = `নম্বর কপি হয়েছে :${hotlineNumber}`
+        alert(message)
+    }
+    if (e.target.className.includes("fa-heart")) {
+        const heartCount = getElement("heart").innerText
+
+        const currentHeartCount = Number(heartCount) + Number(1)
+        getElement("heart").innerText = currentHeartCount
+    }
+})
+
+
+const clearHistoryButton = getElement("clear-btn")
+clearHistoryButton.addEventListener("click", function () {
+    historyContainer.innerHTML = ""
 })
